fcc015fb8cbdf8054aef404c39edfa34398a14bb
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-08 21:08:21 -0600

    feat: add responsive styling changes

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -3,20 +3,8 @@ import './MovieCard.css'
 
 function MovieCard(props) {
   return (
-    <img className='movie-card' src={props.poster} alt='A poster of the movie' onClick={() => props.findMovie(props.id)} ></img>
+    <img className='front-movie-card' src={props.poster} alt='A poster of the movie' onClick={() => props.findMovie(props.id)} ></img>
   )
 }
-
-// function MovieCard(props) {
-//   return (
-//     <img className='movie-card' src={props.poster} id={props.id} onClick = {event => props.findMovie(event.target.id)}></img>
-//   )
-// }
-
-// function MovieCard(props, findMovie) {
-//   return (
-//     <img className='movie-card' src={props.poster} id={props.id} onClick = {event => console.log(event.target.id)}></img>
-//   )
-// }
 
 export default MovieCard
