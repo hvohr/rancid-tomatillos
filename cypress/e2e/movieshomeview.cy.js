eb7e3e5871fde4c9211f07e7ac20aeb946909c0b
Victor Sriqui <victorsriqui@gmail.com>
2023-07-11 07:42:10 -0600

    test: Should test that there is a rendered collection of movies, they are visible img elements, and would display first/last consistently

--- cypress/e2e/movieshomeview.cy.js
+++ cypress/e2e/movieshomeview.cy.js
@@ -11,7 +11,30 @@ describe('Movies home should be tested', () => {
     cy.get("h1").contains("Rancid Tomatillos")
   
   });
-  
-})
+
+  it("should have a collection of movies", () => {
+    cy.get("main")
+    .find("img")
+    .should("have.length", 40);
+  });
+  it("should have a collection of movies in proper order", () => {
+    cy.get("img")
+    .last()
+    .invoke('attr', 'src')
+    .should('eq', 'https://image.tmdb.org/t/p/original//woTQx9Q4b8aO13jR9dsj8C9JESy.jpg')
+    cy.get("img")
+    .first()
+    .invoke('attr', 'src')
+    .should('eq', 'https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg')
+  });
+  it("the img element should be visible for individual movies", () => {
+    cy.get("img")
+    .last()
+    .should('be.visible')
+    cy.get("img")
+    .first()
+    .should('be.visible')
+  });
+});
 
 
