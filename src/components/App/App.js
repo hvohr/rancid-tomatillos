f611be519b64bbcc67ccf460839756a272f21cad
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-12 23:57:02 -0600

    feat: finish router and add api elements and styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,49 +6,37 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import Home from '../../pages/Home'
+import {fetchData, fetchSingleData} from './apiCalls'
 
 function App() {
-  async function fetchData() {
-    try {
-      let response = await fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-      setError(response.status)
-      let responseJson = await response.json();
-      return responseJson;
-    } catch (error) {
-      handleError()
-    }
-  }
   useEffect(() => {
     fetchData().then(
       data => setMovies(data.movies)
     )
   })
-  function handleError() {
-    if (error !== '') {
-      throw new Error(`HTTP Error: ${error} -- Please try again`)
-    }
-  }
   const [movies, setMovies] = useState([]);
-  const [error, setError] = useState('')
-  const [picked, setPicked] = useState('');
+  const [single, setSingle] = useState([])
 
   let filtered = movies.map(movie => <MovieCard poster={movie.poster_path} title={movie.title} id={movie.id} key={movie.id} findMovie={findMovie} />)
 
-  function findMovie(anID) {
-    let singular = movies.find(x => Number(x.id) === anID)
-    setPicked(singular)
+  function findMovie(id) {
+    fetchSingleData(id).then(
+          data => setSingle(data.movie)
+    )
   }
   useEffect(() => {
-    setPicked(picked)
+    setSingle(single)
   })
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<Home filtered ={filtered}/>} />
-        <Route path='/home' element={<Home filtered ={filtered}/>} />
-        <Route path= '/:id' element={<InduvidualMovie movieImage={picked.backdrop_path} pickedTitle={picked.title} pickedRating={picked.average_rating} pickedDate={picked.release_date}/>} />
-      </Routes>
+        <Route path='/' element={<Home filtered={filtered} />} />
+        <Route path='/home' element={<Home filtered={filtered} />} />
+        <Route path='/:id' element={<InduvidualMovie image={single.backdrop_path} title={single.title} 
+        rating={single.average_rating} date={single.release_date} budget={single.budget} overview={single.overview}
+        runtime={single.runtime} tagline={single.tagline} revenue={single.revenue} genres={single.genres} />} />
+      </Routes> 
     </div>
   );
 }
---
+++ src/components/App/apiCalls.js
@@ -0,0 +1,20 @@
+async function fetchData() {
+    let response = await fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+    if (response.ok === false) {
+      throw new Error(response.statusText)
+    }
+    let data = await response.json()
+    return data
+  } 
+
+async function fetchSingleData(id) {
+    let response = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+    if (response.ok === false) {
+      throw new Error(response.statusText)
+    }
+    let data = await response.json();
+    return data;
+  } 
+
+
+export { fetchData, fetchSingleData }
